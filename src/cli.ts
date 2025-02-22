214d0ae8a80ea33b653ea0abcf5790faa2b64dc5
Jens Rømer Hesselbjerg <jh.roemer@gmail.com>
2025-02-22 15:12:54 +0100

    Save response to ts-file

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,5 @@
+import path from "node:path";
+import fs from "node:fs";
 import { intro, outro, text } from "@clack/prompts";
 import cac from "cac";
 
@@ -6,20 +8,37 @@ const cli = cac("generate-mock-data");
 cli.command("").action(async () => {
   intro(`Generate mock data`);
 
+  const method = "GET";
+
   const url = (await text({
     message: `Enter URL`,
   })) as Awaited<string>;
 
-  const fetchResponse = await fetch(url);
-  const data = await fetchResponse.json();
+  const response = await fetch(url);
+  const parsedResponse = await response.json();
 
-  if (!fetchResponse.ok) {
-    outro(
-      `Failed to fetch data from ${url} with status ${fetchResponse.status}`
-    );
+  if (!response.ok) {
+    outro(`Failed to fetch data from ${url} with status ${response.status}`);
   }
 
-  console.log(data);
+  const fileName = "mockData";
+  const variableName = "mockData";
+
+  const filePath = path.join(".", `${fileName}.ts`);
+
+  const fileContent = `const ${variableName} = {
+      method: '${method}',
+      url: '${url}',
+      statusCode: ${response.status},
+      response: ${JSON.stringify(parsedResponse)},
+    };
+
+    export default ${variableName};
+  `;
+
+  fs.writeFileSync(filePath, fileContent);
+
+  outro("File created successfully!");
 });
 
 cli.help();
